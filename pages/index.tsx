e653b3eec2bb8b079b9766d630192ecb39a89131
David Dodda <david@blackbeard.io>
2024-06-17 02:35:15 +0530

    added some placeholder menue items

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,8 @@ export default function Home() {
   const [theme, setTheme] = useState("light");
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
+  const [hoverText, setHoverText] = useState("Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, tempora repellat natus totam quos illum obcaecati voluptates. Mollitia dicta ipsa debitis adipisci autem est amet, fugit aliquam eius quasi numquam!");
+
   const [navOpen, setNavOpen] = useState(false);
 
   const [textColor, setTextColor] = useState("#fff");
@@ -108,38 +110,6 @@ export default function Home() {
       </div>
 
 
-      {/* main background image */}
-      {/* <div className={
-  css`
-    background-image: url(${bgimage.src});    
-    // when in dark mode change the background image into negetive
-    filter: ${theme === "dark" ? "invert(1)" : "none"};
-    background-size: cover;
-    background-position: center;
-    height: 100vh;
-    position: relative;
-    z-index: 1;
-    border-radius: 0.2rem;
-
-    // if nav is open make the width to 30% and height to 70% and position to fixed at the center
-    ${
-      navOpen && css`
-        width: 30%;
-        height: 70%;
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: 0.2rem;
-      `
-    }
-
-  `
-}> */}
-
-
-
-      {/* </div> */}
 
       {/* nav bat */}
       <div className={
@@ -150,7 +120,7 @@ export default function Home() {
       right: 0;
       padding: 1rem;
       font-size: 1.5rem;
-      z-index: 1;
+      z-index: 3;
       color: ${textColor}
       ;
     `
@@ -238,6 +208,142 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* nav menu */}
+      {navOpen && (
+        <div
+          className={
+            css`
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 2;
+            background: ${theme === "light" ? "#fff0" : "#0000"};
+            display: flex;
+            justify-content: space-between;
+            padding: 1rem;
+            color: ${textColor};
+            border-radius: 0.5rem;
+            width: 100vw;
+            height: 100vh;
+            gap: 1rem;
+            overflow-y: auto;
+            overflow-x: hidden;
+            transition: 0.5s;
+            align-items: center;
+          `
+          }
+
+        >
+          <div
+            className={css`
+          background-image: url(${bgimage.src});
+          filter: ${theme === "dark" ? "invert(1)" : "none"};
+          background-size: cover;
+          background-position: center;
+          height: 100vh;
+          position: relative;
+          z-index: 1;
+          border-radius: 0.2rem;
+          transition: filter 0.5s ease, width 0.5s ease, height 0.5s ease, top 0.5s ease, left 0.5s ease, transform 0.5s ease;
+
+          ${navOpen &&
+              css`
+            width: 30%;
+            height: 70%;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: 0.2rem;
+          `}
+        `}
+          ></div>
+          {/* left menu */}
+          <div
+            className={css`
+          display: flex;
+          flex-direction: column;
+          gap: 1rem;
+          width: 30%;
+          padding: 1rem;
+          border-radius: 0.5rem;
+        `}
+          >
+            <div
+              className={css`
+            padding: 0.5rem;
+            font-weight: bold;
+            font-size: 1.2rem;
+            cursor: pointer;
+            transition: background 0.3s;
+            &:hover {
+              background: rgba(0, 0, 0, 0.1);
+            }
+          `}
+              onMouseEnter={() => setHoverText("👨‍💻 Code conjurer by day, idea alchemist by night. Turning wild ideas into cool projects since '95. Let's build something amazing!")}
+            >
+              About
+            </div>
+            <div
+              className={css`
+            padding: 0.5rem;
+            font-weight: bold;
+            font-size: 1.2rem;
+            cursor: pointer;
+            transition: background 0.3s;
+            &:hover {
+              background: rgba(0, 0, 0, 0.1);
+            }
+          `}
+              onMouseEnter={() => setHoverText("Why don’t scientists trust atoms? Because they make up everything!")}
+            >
+              Random
+            </div>
+            <div
+              className={css`
+            padding: 0.5rem;
+            font-weight: bold;
+            font-size: 1.2rem;
+            cursor: pointer;
+            transition: background 0.3s;
+            &:hover {
+              background: rgba(0, 0, 0, 0.1);
+            }
+          `}
+              onMouseEnter={() => setHoverText("Grace alone, faith alone, Christ alone. In the darkest valleys or the highest peaks, remember that our strength and salvation come from God alone. Soli Deo Gloria.")}
+            >
+              The Real Shit
+            </div>
+          </div>
+          {/* right menu */}
+          <div
+            className={css`
+          display: flex;
+          flex-direction: column;
+          gap: 1rem;
+          width: 30%;
+          padding: 1rem;
+          text-align: right;
+        `}
+          >
+            <div
+              className={css`
+            padding: 0.5rem;
+            line-height: 1.5;
+            font-size: 2rem;
+            color: ${textColor};
+          `}
+            >
+              {hoverText}
+            </div>
+          </div>
+        </div>
+      )}
+
+
       {/* photo credits */}
       <div className={
         css`
