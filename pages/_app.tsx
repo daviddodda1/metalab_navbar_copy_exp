7f8bd5814e40715461517d1eaa2fc65a659e916a
David Dodda <david@blackbeard.io>
2024-06-17 02:16:46 +0530

    the core animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import React from "react";
+
+import "@/styles/globals.css";
+
+
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
